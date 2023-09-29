3d7c3686113d03e5e82ab7936f70c054570843c7
Cristiane Abreu <bjwmcd@yahoo.com>
2023-09-29 14:43:44 -0400

    creating the project informations

---
+++ react-updates/src/components/Projects/projectData.js
@@ -0,0 +1,52 @@
+export default [
+    {
+        id: 1,
+        title: "AirBnb",
+        description: "Created white ReactJs, HTML, CSS, ",
+        technologies: 136,
+        coverImg: "katie-zaferes.png",
+        link: "Online",
+        
+    },
+    {
+        id: 2,
+        title: "Netflix",
+        description: "Created white ReactJs, HTML, CSS, ",
+        technologies: 136,
+        coverImg: "katie-zaferes.png",
+        link: "Online",
+       
+    },
+    {
+        id: 3,
+        title: "Affordable Website",
+        description: "Created white ReactJs, HTML, CSS, ",
+        technologies: 136,
+        coverImg: "katie-zaferes.png",
+        link: "Online",
+    },
+    {
+        id: 4,
+        title: "Sample",
+        description: "Created white ReactJs, HTML, CSS, ",
+        technologies: 136,
+        coverImg: "katie-zaferes.png",
+        link: "Online",
+    },
+    {
+        id: 5,
+        title: "Sample2",
+        description: "Created white ReactJs, HTML, CSS, ",
+        technologies: 136,
+        coverImg: "katie-zaferes.png",
+        link: "Online",
+    },
+    {
+        id: 6,
+        title: "Sample3",
+        description: "Created white ReactJs, HTML, CSS, ",
+        technologies: 136,
+        coverImg: "katie-zaferes.png",
+        link: "Online",
+    }
+]
