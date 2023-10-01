0d15aeabb81663c2f18223b52218b02a51225368
Cristiane Abreu <bjwmcd@yahoo.com>
2023-10-01 19:51:48 -0400

    update

---
+++ react-updates/src/components/Projects/Project.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function project(){
+    return(
+        <div>
+             <img src={avatar_url} alt=""className="project-image" />
+                <h3>T</h3>
+                <div>
+                    <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
+                </div>
+        </div>
+    )
+};
